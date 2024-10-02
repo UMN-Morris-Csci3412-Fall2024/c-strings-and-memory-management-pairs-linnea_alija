5fbbb0be8b9d5b9c43c06f5272eb183287a98734
alijawosti <alijawosti@gmail.com>
2024-10-02 10:49:15 -0500

    Updated main.c to properly handle and free memory after using disemvowel function
    Co-authored by LInnea Gilbertson <gilb0348@morris.umn.edu>

--- disemvowel/main.c
+++ disemvowel/main.c
@@ -1,16 +1,20 @@
 #include <stdio.h>
 #include <stdlib.h>
-
+#include <string.h>
 #include "disemvowel.h"
 
 int main(int argc, char *argv[]) {
-  char *line;
-  size_t size;
-  
-  size = 100;
-  line = (char*) malloc (size + 1);
+    char *line = NULL;  // getline will allocate memory
+    size_t size = 0;
+    char *disemvoweled;
 
-  while (getline(&line, &size, stdin) > 0) {
-    printf("%s\n", disemvowel(line));
-  }
+    // Read input line by line
+    while (getline(&line, &size, stdin) > 0) {
+        disemvoweled = disemvowel(line);  // Call disemvowel
+        printf("%s\n", disemvoweled);     // Print the disemvoweled string
+
+        free(disemvoweled);               // Free memory allocated by disemvowel
+    }
+
+    free(line);  // Free memory allocated by getline
 }
